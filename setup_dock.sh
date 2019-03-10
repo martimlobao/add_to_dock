6eb4175653c4693339f1cccbc0b5a20713f54488
Martim Lobao <m@rtimlobao.com>
2019-03-10 11:31:07 -0400

    configure Dock to personal preferences

--- setup_dock.sh
+++ setup_dock.sh
@@ -3,18 +3,9 @@ source ./add_to_dock.sh
 # WARNING: clears existing dock
 clear_dock
 
-add_app_to_dock "System Preferences"
-add_app_to_dock "Activity Monitor"
-add_spacer_to_dock
-add_app_to_dock "Microsoft Outlook"
 add_app_to_dock "Google Chrome"
-add_app_to_dock "Microsoft Excel"
-add_app_to_dock "Microsoft Word"
-add_app_to_dock "Microsoft PowerPoint"
-add_app_to_dock "kitty"
-add_app_to_dock "Visual Studio Code"
-add_app_to_dock "SelfControl"
-add_app_to_dock "Skype"
-add_spacer_to_dock
+add_app_to_dock "Atom"
+add_app_to_dock "System Preferences"
+add_folder_to_dock /Downloads --sortby 2 --displayas 1 --viewcontentas 1
 
 killall Dock
